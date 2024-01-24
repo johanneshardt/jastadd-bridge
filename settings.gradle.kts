1dd2bb005c6bbc87acac790d793fea71617a3ade
johanneshardt <me@johanneshardt.com>
2024-01-24 09:28:17 +0100

    fix jdk toolchain resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,17 @@ include("server")
 include("interop")
 include("CalcRAG")
 project(":CalcRAG").projectDir = file("examples/CalcRAG")
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "0.8.0"
+}
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass = org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java
+            }
+        }
+    }
+}
